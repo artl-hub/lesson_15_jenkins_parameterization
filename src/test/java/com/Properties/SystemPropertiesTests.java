dfefcd85eaa3a9e3463f7afb990700a918f07c77
artl <33ttpp@gmail.com>
2024-01-16 22:06:01 +0100

    added hello test

--- src/test/java/com/Properties/SystemPropertiesTests.java
+++ src/test/java/com/Properties/SystemPropertiesTests.java
@@ -3,6 +3,8 @@ package com.Properties;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static java.lang.String.format;
+
 public class SystemPropertiesTests {
 
     @Test
@@ -47,6 +49,23 @@ public class SystemPropertiesTests {
 
         // gradle property_test -Dbrowser=opera
         // opera
+    }
+
+    @Test
+    @Tag("hello")
+    void systemProperties5Test() {
+        String name = System.getProperty("name", "default student");
+        String message = format("Hello, %s!", name);
+
+        System.out.println(message); //
+        //gradle hello_test
+        //Hello, default student!
+
+        //gradle hello_test "-Dname=Alex Egorov"
+        //gradle hello_test -Dname="Alex Egorov"
+        //if gradle hello_test -Dname=Alex Egorov  - will be BUILD FAILED: Task 'Egorov' not found in foot project
+
+
     }
 
 
