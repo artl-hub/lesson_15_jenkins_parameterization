48eb6a999382c34dbcf9dbf3ddddeb77db50a800
artl <33ttpp@gmail.com>
2024-01-31 17:04:32 +0100

    added allure-notifications

--- src/test/java/com/tests/RegistrationRemoteTest.java
+++ src/test/java/com/tests/RegistrationRemoteTest.java
@@ -33,6 +33,7 @@ public class RegistrationRemoteTest {
         Configuration.browserVersion = System.getProperty("browserVersion", "chrome (110.0)");
         Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
         Configuration.remote = "https://user1:1234@" + System.getProperty("wdHost", "selenoid.autotests.cloud/wd/hub");
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
